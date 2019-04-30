b1433a1df5e53b4291373660180174b0309cb339
stevenong99 <49690818+stevenong99@users.noreply.github.com>
2019-04-30 15:02:24 +0800

    Update CrabFood.java

--- CrabFood.java
+++ CrabFood.java
@@ -1,5 +1,6 @@
 package crabfood;
 
+import Map.Map;
 import TextReaders.Customertxtreader;
 import TextReaders.Inputtxtreader;
 
@@ -10,9 +11,8 @@ import TextReaders.Inputtxtreader;
 public class CrabFood {
 
     public static void main(String[] args) {
-        Customertxtreader customer = new Customertxtreader("Customer.txt");
-        System.out.println("");
-        Inputtxtreader input = new Inputtxtreader("Input.txt");
+        Customertxtreader customer = new Customertxtreader("Customer.txt", "Input.txt");
     }
     
 }
+
