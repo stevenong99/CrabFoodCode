289aed9f7a854be6014bf5e46cf8e05f9d5a8a23
stevenong99 <49690818+stevenong99@users.noreply.github.com>
2019-04-30 15:06:18 +0800

    Update Restaurants.java

--- Restaurants.java
+++ Restaurants.java
@@ -4,11 +4,13 @@ public class Restaurants {
     private String name;
     private int[][] branch;
     private Dishes[] dishes;
+    private int numberofbranches;
     
     public Restaurants(String name,int[][] branch, Dishes[] dishes){
         this.name=name;
         this.branch=branch;
         this.dishes=dishes;
+        this.numberofbranches = 3;
     }
     
     @Override
@@ -41,13 +43,42 @@ public class Restaurants {
         }else return'0';
     }
     
-    public int getTime(Dishes dish){
+    public int getTime(String dishname){
         int i=0;
         while (i<dishes.length){
-            if (dish.getDishName().equals(dishes[i].getDishName())) 
+            if (dishname.equals(dishes[i].getDishName())) 
                break;
             i++;
         }
         return dishes[i].getPreparationTime();
     }
+
+    public void setBranch(int[][] branch) {
+        this.branch = branch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int closestBranch(double customerlocationX, double customerlocationY){
+        int restX = branch[0][0];
+        int restY = branch[0][1];
+        int closestX = 0;
+        int closestY = 0;
+        int time = (int) Math.pow(customerlocationX - restX, 2) + (int) Math.pow(customerlocationY - restY, 2);
+        for (int i = 1; i < numberofbranches; i++) {
+            restX = branch[i][0];
+            restY = branch[i][1];
+            int newTime = (int) Math.pow(customerlocationX - restX, 2) + (int) Math.pow(customerlocationY - restY, 2);
+            if (newTime< time){
+                time = newTime;
+                closestX = restX;
+                closestY = restY;
+            }
+        }
+        int deliverytime = (int)(Math.abs(customerlocationX - closestX) + Math.abs(customerlocationY - closestY));
+        return deliverytime;
+    }
+    
 }
