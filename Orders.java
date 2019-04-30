f09ba58954d95e3010a8ea34443b3509b2ce3ae0
stevenong99 <49690818+stevenong99@users.noreply.github.com>
2019-04-30 15:05:44 +0800

    Update Orders.java

--- Orders.java
+++ Orders.java
@@ -1,12 +1,19 @@
 package Data;
 
-public class Orders {
+public class Orders{
     private String resname, dishname;
-    private int arrivaltime;
-    public Orders(int arrivaltime, String resname, String dishname) {
+    private int arrivaltime, finishedcookingtime, timetakentocook, deliverytime, totaltime, timedeliveredtocustomer, customerlocationX, customerlocationY;
+    public Orders(int arrivaltime, String resname, String dishname, int timetakentocook, int deliverytime, int customerlocationX, int customerlocationY) {
         this.arrivaltime = arrivaltime;
         this.resname = resname;
         this.dishname = dishname;
+        this.customerlocationX = customerlocationX;
+        this.customerlocationY = customerlocationY;
+        this.timetakentocook = timetakentocook;
+        this.finishedcookingtime = arrivaltime + timetakentocook;
+        this.deliverytime = deliverytime;
+        this.totaltime = deliverytime + timetakentocook;
+        this.timedeliveredtocustomer = totaltime + arrivaltime;
     }
 
     public int getArrivaltime() {
@@ -34,7 +41,9 @@ public class Orders {
     }
 
     public String toString(int i) {
-        return "Order " + i +  "\nRestaurant name = " + resname + "\nDish name = " + dishname + "\nArrival time = " + arrivaltime;
+        return "Order " + i +  "\nRestaurant name = " + resname + "\nDish name = " + dishname + "\nArrival time = " + arrivaltime
+                + "\nFinished cooking at = " + finishedcookingtime + "\nDelivered at = " + timedeliveredtocustomer;
+        
     }
     
     
